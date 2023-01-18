bc9e5e332ea1f7d938b63d3b64247c573ea25ff4
Vladimir <v.khliupnev@haulmont.com>
2023-01-18 19:59:21 +0400

    add gh pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import {defineNuxtConfig} from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  router: {
+    base: '/my-pet-projects/'
+  },
   modules: ["@nuxt/image-edge"],
   css: ["~/assets/styles/tailwind.css", "~/assets/styles/main.css"],
   build: {
