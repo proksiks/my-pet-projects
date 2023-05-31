a142eb70d479fbbc9f9b1e8389c8573a05ee6e9c
Vladimir <v.khliupnev@haulmont.com>
2023-05-31 19:42:42 +0400

    add SSG

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  ssr: false,
+  app: {
+    head: {
+      title: "Nuxt3 GitHub Pages",
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "Test" },
+        { name: "format-detection", content: "telephone=no" },
+      ],
+      link: [{ rel: "icon", type: "image/svg", href: "/favicon.ico" }],
+    },
+    baseURL: "/my-pet-projects/",
+  },
+});
