c285141a2cad15d5c57afb37638f4620391dcf9f
Vladimir <v.khliupnev@haulmont.com>
2023-06-14 16:02:36 +0400

    Updates

--- _nuxt/index.9d45574e.js
+++
@@ -1,1 +0,0 @@
-import{a as s,b as o,o as t,h as e}from"./entry.7ab0c61b.js";const c={},n={class:"wrap"},a=e("h1",null,"Информация",-1),_=e("p",null,"Тут должны быть информация обо мне с красивым дизайном(это будет позже)",-1),r=[a,_];function d(i,p,l,h,f,u){return t(),o("div",n,r)}const x=s(c,[["render",d]]);export{x as default};
